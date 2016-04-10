02ffc1c84ecc4a4d5a3786a59321459556d9b758
Vadim Gilem <agis366@mail.ru>
2016-04-10 20:00:02 +0500

    General project's script

---
+++ WebUI/Models/GamesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+
+using Domain.Entities;
+
+namespace WebUI.Models
+{
+    public class GamesListViewModel
+    {
+        public IEnumerable<Game> Games { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
